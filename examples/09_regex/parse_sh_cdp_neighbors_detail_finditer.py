706e29fa344be9fe5f1a71ce03495251dc02a3fb
natenka <nataliya.samoylenko@gmail.com>
2017-07-11 19:23:06 +0000

    Новый пример parse_sh_cdp_neighbors_detail_finditer.py

---
+++ examples/09_regex/parse_sh_cdp_neighbors_detail_finditer.py
@@ -0,0 +1,25 @@
+import re
+from pprint import pprint
+
+
+def parse_cdp(filename):
+    regex = ('Device ID: (?P<device>\S+)'
+             '|Entry address.*\n +IP address: (?P<ip>\S+)'
+             '|Platform: (?P<platform>\S+ \S+),'
+             '|Cisco IOS Software, (?P<ios>.+), RELEASE')
+
+    result = {}
+
+    with open('sh_cdp_neighbors_sw1.txt') as f:
+        match_iter = re.finditer(regex, f.read())
+        for match in match_iter:
+            if match.lastgroup == 'device':
+                device = match.group(match.lastgroup)
+                result[device] = {}
+            elif device:
+                result[device][match.lastgroup] = match.group(match.lastgroup)
+
+    return result
+
+pprint(parse_cdp('sh_cdp_neighbors_sw1.txt'))
+
