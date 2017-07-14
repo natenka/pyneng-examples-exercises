0e0f0dea71508433e6aa44b26944532d53cf8b43
natenka <nataliya.samoylenko@gmail.com>
2017-07-14 07:14:17 +0000

    Update sqlite examples

--- examples/11_db/create_sw_inventory_ver2.py
+++ examples/11_db/create_sw_inventory_ver2.py
@@ -6,12 +6,20 @@ data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
         ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
         ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
 
-with sqlite3.connect('sw_inventory3.db') as con:
+con = sqlite3.connect('sw_inventory3.db')
+con.execute("create table switch "
+            "(mac text primary key, hostname text,"
+             "model text, location text)")
 
-    con.execute("create table switch (mac text primary key, hostname text, model text, location text)")
+try:
+    with con:
+        query = "INSERT into switch values (?, ?, ?, ?)"
+        con.executemany(query, data)
 
-    query = "INSERT into switch values (?, ?, ?, ?)"
-    con.executemany(query, data)
+except sqlite3.IntegrityError as e:
+    print("Error occured: ", e)
 
-    for row in con.execute("select * from switch"):
-        print(row)
+for row in con.execute("select * from switch"):
+    print(row)
+
+
--- examples/11_db/create_sw_inventory_ver3.py
+++ examples/11_db/create_sw_inventory_ver3.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 import sqlite3
 
 data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
@@ -6,33 +7,48 @@ data = [('0000.AAAA.CCCC', 'sw1', 'Cisco 3750', 'London, Green Str'),
         ('0000.AAAA.DDDD', 'sw3', 'Cisco 2960', 'London, Green Str'),
         ('0011.AAAA.CCCC', 'sw4', 'Cisco 3750', 'London, Green Str')]
 
-con = sqlite3.connect('sw_inventory3.db')
-con.execute("create table switch (mac text primary key, hostname text, model text, location text)")
+#MAC-адрес sw7 совпадает с MAC-адресом существующего коммутатора - sw3
+data2 = [('0000.AAAA.DDDD', 'sw7', 'Cisco 2960', 'London, Green Str')]
 
-try:
-    with con:
-        query = "INSERT into switch values (?, ?, ?, ?)"
-        con.executemany(query, data)
-except sqlite3.IntegrityError as e:
-    print("Error occured: ", e)
+def crete_connection(db_name):
+    con = sqlite3.connect(db_name)
+    return con
 
-for row in con.execute("select * from switch"):
-    print(row)
+
+def create_table(connection, sql_command):
+    con.execute("""create table switch
+                   (mac text primary key,
+                    hostname text,
+                    model text,
+                    location text)""")
+
+
+def write_data_to_db(connection, query, data):
+    try:
+        with connection:
+            connection.executemany(query, data)
+    except sqlite3.IntegrityError as e:
+        print("Error occured: ", e)
+        return False
+    else:
+        return True
+
+
+def get_all(connection, query):
+    result = []
+    for row in connection.execute(query):
+        result.append(row)
+    return result
+
+
+query = "INSERT into switch values (?, ?, ?, ?)"
+
+write_data_to_db(query, data)
+pprint(get_all("select * from switch"))
 
 print('-'*30)
 
-#MAC-адрес sw7 совпадает с MAC-адресом существующего коммутатора - sw3
-data2 = [('0055.AAAA.CCCC', 'sw5', 'Cisco 3750', 'London, Green Str'),
-         ('0066.BBBB.CCCC', 'sw6', 'Cisco 3780', 'London, Green Str'),
-         ('0000.AAAA.DDDD', 'sw7', 'Cisco 2960', 'London, Green Str'),
-         ('0088.AAAA.CCCC', 'sw8', 'Cisco 3750', 'London, Green Str')]
+write_data_to_db(query, data2)
+pprint(get_all("select * from switch"))
 
-try:
-    with con:
-        query = "INSERT into switch values (?, ?, ?, ?)"
-        con.executemany(query, data2)
-except sqlite3.IntegrityError as e:
-    print("Error occured: ", e)
 
-for row in con.execute("select * from switch"):
-    print(row)
