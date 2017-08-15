087a1fd7bf1ccf6eb52b1af2a426c24d38d2e670
natenka <nataliya.samoylenko@gmail.com>
2017-08-15 12:17:01 +0000

    Update exercise 03_data_structures description

--- exercises/03_data_structures/task_3_6.py
+++ exercises/03_data_structures/task_3_6.py
@@ -2,7 +2,11 @@
 '''
 Задание 3.6
 
-Обработать строку NAT таким образом, чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
+Обработать строку NAT таким образом,
+чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
 '''
 
 NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
