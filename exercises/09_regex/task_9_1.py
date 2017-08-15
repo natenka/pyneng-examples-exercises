ef343ece446ce8f591e45bc7fc94393aa20796ba
natenka <nataliya.samoylenko@gmail.com>
2017-08-15 12:24:05 +0000

    Update exercise 09_regex description

--- exercises/09_regex/task_9_1.py
+++ exercises/09_regex/task_9_1.py
@@ -4,8 +4,8 @@
 Задание 9.1
 
 Создать скрипт, который будет ожидать два аргумента:
-    1. имя файла, в котором находится вывод команды show
-    2. регулярное выражение
+1. имя файла, в котором находится вывод команды show
+2. регулярное выражение
 
 В результате выполнения скрипта, на стандартный поток вывода должны быть
 выведены те строки из файла с выводом команды show,
@@ -13,4 +13,30 @@
 
 Проверить работу скрипта на примере вывода команды sh ip int br (файл sh_ip_int_br.txt).
 Например, попробуйте вывести информацию только по интерфейсу FastEthernet0/1.
+
+Работа этого скрипта должна имитировать работу фильтра include в Cisco.
+Пример работы скрипта:
+
+$ python task_9_1.py sh_ip_int_br.txt "Fas"
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+FastEthernet0/2            10.0.13.1       YES manual up                    up
+FastEthernet0/3            unassigned      YES unset  up                    down
+
+$ python task_9_1.py sh_ip_int_br.txt "manual"
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+FastEthernet0/2            10.0.13.1       YES manual up                    up
+Loopback0                  10.1.1.1        YES manual up                    up
+Loopback100                100.0.0.1       YES manual up                    up
+
+$ python task_9_1.py sh_ip_int_br.txt "up +up"
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+FastEthernet0/2            10.0.13.1       YES manual up                    up
+Loopback0                  10.1.1.1        YES manual up                    up
+Loopback100                100.0.0.1       YES manual up                    up
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
 '''
