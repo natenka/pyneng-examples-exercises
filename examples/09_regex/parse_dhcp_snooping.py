4358e09fdd1bd4dde5fb85351fadc7764c15ed2d
natenka <nataliya.samoylenko@gmail.com>
2017-08-16 04:28:23 +0000

    Обновлены примеры раздела 09_regex

--- examples/09_regex/parse_dhcp_snooping.py
+++ examples/09_regex/parse_dhcp_snooping.py
@@ -11,15 +11,15 @@ with open('dhcp_snooping.txt') as data:
         if match:
             result.append(match.groupdict())
 
-print("К коммутатору подключено {} устройства".format(len(result)))
+print('К коммутатору подключено {} устройства'.format(len(result)))
 
 for num, comp in enumerate(result, 1):
-    print("Параметры устройства {}:".format(num))
+    print('Параметры устройства {}:'.format(num))
     for key in comp:
-        print("{:10}: {:10}".format(key,comp[key]))
+        print('{:10}: {:10}'.format(key,comp[key]))
 
 
-"""
+'''
 Example:
 
 $ python parse_dhcp_snooping.py
@@ -45,4 +45,4 @@ ip        : 10.1.10.6
 vlan      : 10
 int       : FastEthernet0/3
 
-"""
+'''
