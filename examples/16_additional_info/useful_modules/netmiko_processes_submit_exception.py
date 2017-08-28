17cc0fe6afa87c721b5ede044d212d4736443bac
natenka <nataliya.samoylenko@gmail.com>
2017-08-28 06:55:08 +0000

    Add new subchapter: Module concurrent.futures

---
+++ examples/16_additional_info/useful_modules/netmiko_processes_submit_exception.py
@@ -0,0 +1,48 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from pprint import pprint
+from datetime import datetime
+import time
+from itertools import repeat
+
+import yaml
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoAuthenticationException
+
+
+start_msg = '===> {} Connection to device: {}'
+received_msg = '<=== {} Received result from device: {}'
+
+
+def connect_ssh(device_dict, command):
+    print(start_msg.format(datetime.now().time(), device_dict['ip']))
+    if device_dict['ip'] == '192.168.100.1':
+        time.sleep(10)
+    with ConnectHandler(**device_dict) as ssh:
+        ssh.enable()
+        result = ssh.send_command(command)
+        print(received_msg.format(datetime.now().time(), device_dict['ip']))
+    return {device_dict['ip']: result}
+
+
+def processes_conn(function, devices, limit=2, command=''):
+    all_results = {}
+    with ProcessPoolExecutor(max_workers=limit) as executor:
+        future_ssh = [executor.submit(function, device, command)
+                      for device in devices]
+        for f in as_completed(future_ssh):
+            try:
+                result = f.result()
+            except NetMikoAuthenticationException as e:
+                print(e)
+            else:
+                all_results.update(result)
+    return all_results
+
+
+if __name__ == '__main__':
+    devices = yaml.load(open('devices.yaml'))
+    all_done = processes_conn(connect_ssh,
+                              devices['routers'],
+                              command='sh clock')
+    pprint(all_done)
+
--- examples/16_additional_info/useful_modules/netmiko_threads_submit.py
+++ examples/16_additional_info/useful_modules/netmiko_threads_submit.py
@@ -1,26 +1,35 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pprint import pprint
+from datetime import datetime
+import time
+from itertools import repeat
 
 import yaml
 from netmiko import ConnectHandler
 
 
+start_msg = '===> {} Connection to device: {}'
+received_msg = '<=== {} Received result from device: {}'
+
+
 def connect_ssh(device_dict, command):
-    print('Connection to device {}'.format(device_dict['ip']))
+    print(start_msg.format(datetime.now().time(), device_dict['ip']))
+    if device_dict['ip'] == '192.168.100.1':
+        time.sleep(10)
     with ConnectHandler(**device_dict) as ssh:
         ssh.enable()
         result = ssh.send_command(command)
-
+        print(received_msg.format(datetime.now().time(), device_dict['ip']))
     return {device_dict['ip']: result}
 
 
-def threads_conn(function, devices, limit=2, **kwargs):
-    all_results = {}
+def threads_conn(function, devices, limit=2, command=''):
+    all_results = []
     with ThreadPoolExecutor(max_workers=limit) as executor:
-        future_ssh = [executor.submit(function, device, **kwargs)
+        future_ssh = [executor.submit(function, device, command)
                       for device in devices]
         for f in as_completed(future_ssh):
-            all_results.update(f.result())
+            all_results.append(f.result())
     return all_results
 
 
@@ -28,7 +37,6 @@ if __name__ == '__main__':
     devices = yaml.load(open('devices.yaml'))
     all_done = threads_conn(connect_ssh,
                             devices['routers'],
-                            limit=3,
-                            command='sh ip int br')
+                            command='sh clock')
     pprint(all_done)
 
