be995a15567ffaa863384b98a5c5b7630be5e71a
natenka <nataliya.samoylenko@gmail.com>
2017-10-14 05:53:02 +0000

    Add DictWriter example

---
+++ examples/10_serialization/csv/csv_write_dict.py
@@ -0,0 +1,27 @@
+import csv
+
+
+data = [{'hostname': 'sw1',
+         'location': 'London',
+         'model': '3750',
+         'vendor': 'Cisco'},
+        {'hostname': 'sw2',
+         'location': 'Liverpool',
+         'model': '3850',
+         'vendor': 'Cisco'},
+        {'hostname': 'sw3',
+         'location': 'Liverpool',
+         'model': '3650',
+         'vendor': 'Cisco'},
+        {'hostname': 'sw4',
+         'location': 'London',
+         'model': '3650',
+         'vendor': 'Cisco'}]
+
+with open('csv_write_dictwriter.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=list(data[0].keys()),
+                            quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+    for d in data:
+        writer.writerow(d)
+
