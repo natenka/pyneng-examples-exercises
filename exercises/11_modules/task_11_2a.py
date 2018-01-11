5eb44854acbbb306afcc78d43fc2378c42ab6886
natenka <nataliya.samoylenko@gmail.com>
2018-01-11 14:48:59 +0000

    Удалила задание 11.1. Сместила нумерацию заданий 11.2. Переместила заданий 11.3 в 12 раздел

--- exercises/11_modules/task_11_2a.py
+++ exercises/11_modules/task_11_2a.py
@@ -3,15 +3,21 @@
 '''
 Задание 11.2a
 
-С помощью функции parse_cdp_neighbors из задания 11.2
+С помощью функции parse_cdp_neighbors из задания 11.1
 и функции draw_topology из файла draw_network_graph.py
 сгенерировать топологию, которая соответствует выводу
-команды sh cdp neighbor в файле sw1_sh_cdp_neighbors.txt
+команды sh cdp neighbor из файлов:
+* sh_cdp_n_sw1.txt
+* sh_cdp_n_r1.txt
+* sh_cdp_n_r2.txt
+* sh_cdp_n_r3.txt
+
 
 Не копировать код функций parse_cdp_neighbors и draw_topology.
 
 В итоге, должен быть сгенерировано изображение топологии.
 Результат должен выглядеть так же, как схема в файле task_11_2a_topology.svg
+
 
 При этом:
 * Интерфейсы могут быть записаны с пробелом Fa 0/0 или без Fa0/0.
