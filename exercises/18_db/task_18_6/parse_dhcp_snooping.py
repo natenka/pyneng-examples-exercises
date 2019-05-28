1faa8274880e2a3145a4efa156806691505d4ecb
natenka <nataliya.samoylenko@gmail.com>
2019-05-28 09:00:11 +0000

    Chapter 18_db exercises. Done

--- exercises/18_db/task_18_6/parse_dhcp_snooping.py
+++ exercises/18_db/task_18_6/parse_dhcp_snooping.py
@@ -9,32 +9,32 @@ DFLT_DB_SCHEMA = 'dhcp_snooping_schema.sql'
 
 
 def create(args):
-    print('Creating DB {} with DB schema {}'.format(args.name, args.schema))
+    print('Создаю БД {} со схемой {}'.format(args.name, args.schema))
     pds.create_db(args.name, args.schema)
 
 
 def add(args):
     if args.sw_true:
-        print('Adding switch data to database')
+        print('Добавляю данные о коммутаторах')
         pds.add_data_switches(args.db_file, args.filename)
     else:
-        print('Reading info from file(s) \n{}'.format(', '.join(
+        print('Читаю информацию из файлов\n{}'.format(', '.join(
             args.filename)))
-        print('\nAdding data to db {}'.format(args.db_file))
+        print('\nДобавляю данные по DHCP записям в {}'.format(args.db_file))
         pds.add_data(args.db_file, args.filename)
 
 
 def get(args):
     if args.key and args.value:
-        print('Geting data from DB: {}'.format(args.db_file))
-        print('Request data for host(s) with {} {}'.format(
-            args.key, args.value))
+        print('Данные из БД: {}'.format(args.db_file))
+        print('Информация об устройствах с такими параметрами:',
+              args.key, args.value)
         pds.get_data(args.db_file, args.key, args.value)
     elif args.key or args.value:
-        print('Please give two or zero args\n')
+        print('Пожалуйста, введите два или ноль аргументов\n')
         print(show_subparser_help('get'))
     else:
-        print('Showing {} content...'.format(args.db_file))
+        print('В таблице dhcp такие записи:')
         pds.get_all_data(args.db_file)
 
 
@@ -50,39 +50,35 @@ def show_subparser_help(subparser_name):
 
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(
-    title='subcommands',
-    description='valid subcommands',
-    help='additional info')
+subparsers = parser.add_subparsers(title='subcommands',
+                                   description='команды',
+                                   help='дополнительная информация')
 
-create_parser = subparsers.add_parser('create_db', help='create new db')
-create_parser.add_argument(
-    '-n', dest='name', default=DFLT_DB_NAME, help='db filename')
-create_parser.add_argument(
-    '-s', dest='schema', default=DFLT_DB_SCHEMA, help='db schema filename')
+create_parser = subparsers.add_parser('create_db', help='создать новую базу данных')
+create_parser.add_argument('-n', dest='name', default=DFLT_DB_NAME,
+                           help='имя БД')
+create_parser.add_argument('-s', dest='schema', default=DFLT_DB_SCHEMA,
+                           help='схема БД')
 create_parser.set_defaults(func=create)
 
-add_parser = subparsers.add_parser('add', help='add data to db')
-add_parser.add_argument('filename', nargs='+', help='file(s) to add to db')
-add_parser.add_argument(
-    '--db', dest='db_file', default=DFLT_DB_NAME, help='db name')
-add_parser.add_argument(
-    '-s',
-    dest='sw_true',
-    action='store_true',
-    help='add switch data if set, else add normal data')
+add_parser = subparsers.add_parser('add', help='добавить данные в БД')
+add_parser.add_argument('filename', nargs='+',
+                        help='файл(ы), которые надо добавить')
+add_parser.add_argument('--db', dest='db_file', default=DFLT_DB_NAME,
+                        help='имя БД')
+add_parser.add_argument('-s', dest='sw_true', action='store_true',
+                        help=('если флаг установлен, добавлять '
+                              'данные коммутаторов, иначе добавлять DHCP записи'))
 add_parser.set_defaults(func=add)
 
-get_parser = subparsers.add_parser('get', help='get data from db')
-get_parser.add_argument(
-    '--db', dest='db_file', default=DFLT_DB_NAME, help='db name')
-get_parser.add_argument(
-    '-k',
-    dest='key',
-    choices=['mac', 'ip', 'vlan', 'interface', 'switch'],
-    help='host key (parameter) to search')
-get_parser.add_argument('-v', dest='value', help='value of key')
-get_parser.add_argument('-a', action='store_true', help='show db content')
+get_parser = subparsers.add_parser('get', help='отобразить данные из БД')
+get_parser.add_argument('--db', dest='db_file', default=DFLT_DB_NAME,
+                        help='имя БД')
+get_parser.add_argument('-k', dest='key',
+                        choices=['mac', 'ip', 'vlan', 'interface', 'switch'],
+                        help='параметр для поиска записей')
+get_parser.add_argument('-v', dest='value', help='значение параметра')
+get_parser.add_argument('-a', action='store_true', help='показать все содержимое БД')
 get_parser.set_defaults(func=get)
 
 if __name__ == '__main__':
--- exercises/18_db/task_18_6/task_18_6.py
+++ exercises/18_db/task_18_6/task_18_6.py
@@ -2,12 +2,13 @@
 '''
 Задание 18.6
 
+Для заданий 18 раздела нет тестов!
+
 В этом задании выложен файл parse_dhcp_snooping.py.
+В файле parse_dhcp_snooping.py нельзя ничего менять.
 
 В файле созданы несколько функций и описаны аргументы командной строки,
 которые принимает файл.
-
-> В файле parse_dhcp_snooping.py нельзя ничего менять.
 
 Есть поддержка аргументов для выполнения всех действий, которые,
 в предыдущих заданиях, выполнялись в файлах create_db.py, add_data.py и get_data.py.
@@ -29,7 +30,7 @@ import parse_dhcp_snooping_functions as pds
 в файле parse_dhcp_snooping.py.
 
 В принципе, для выполнения задания, не обязательно разбираться с модулем argparse.
-Но, вы можете почитать о нем в разделе 08_modules/useful_modules/argparse.html.
+Но, вы можете почитать о нем в разделе https://natenka.gitbook.io/pyneng/part_ii/12_useful_modules/argparse.
 
 Для того, чтобы было проще начать, попробуйте создать необходимые функции в файле
 parse_dhcp_snooping_functions.py и просто выведите аргументы функций, используя print.
@@ -47,23 +48,10 @@ parse_dhcp_snooping_functions.py и просто выведите аргумен
 * добавление информации на основании вывода sh ip dhcp snooping binding из файлов
 * выборку информации из БД (по параметру и всю информацию)
 
-
 Чтобы было проще понять, как будет выглядеть вызов скрипта,
 ниже несколько примеров.
-
-$ python parse_dhcp_snooping.py -h
-usage: parse_dhcp_snooping.py [-h] {create_db,add,get} ...
-
-optional arguments:
-  -h, --help           show this help message and exit
-
-subcommands:
-  valid subcommands
-
-  {create_db,add,get}  additional info
-    create_db          create new db
-    add                add data to db
-    get                get data from db
+В примерах показывается вариант, когда в базе данных есть поля active и last_active,
+но можно также использовать вариант без этих полей.
 
 $ python parse_dhcp_snooping.py get -h
 usage: parse_dhcp_snooping.py get [-h] [--db DB_FILE]
@@ -72,83 +60,100 @@ usage: parse_dhcp_snooping.py get [-h] [--db DB_FILE]
 
 optional arguments:
   -h, --help            show this help message and exit
-  --db DB_FILE          db name
+  --db DB_FILE          имя БД
   -k {mac,ip,vlan,interface,switch}
-                        host key (parameter) to search
-  -v VALUE              value of key
-  -a                    show db content
+                        параметр для поиска записей
+  -v VALUE              значение параметра
+  -a                    показать все содержимое БД
+
 
 $ python parse_dhcp_snooping.py add -h
 usage: parse_dhcp_snooping.py add [-h] [--db DB_FILE] [-s]
                                   filename [filename ...]
 
 positional arguments:
-  filename      file(s) to add to db
+  filename      файл(ы), которые надо добавить
 
 optional arguments:
   -h, --help    show this help message and exit
-  --db DB_FILE  db name
-  -s            add switch data if set, else add normal data
+  --db DB_FILE  имя БД
+  -s            если флаг установлен, добавлять данные коммутаторов, иначе -
+                DHCP записи
 
-$ python parse_dhcp_snooping.py create_db -h
-usage: parse_dhcp_snooping.py create_db [-h] [-n NAME] [-s SCHEMA]
+
+$ python parse_dhcp_snooping.py add -h
+usage: parse_dhcp_snooping.py add [-h] [--db DB_FILE] [-s]
+                                  filename [filename ...]
+
+positional arguments:
+  filename      файл(ы), которые надо добавить
 
 optional arguments:
-  -h, --help  show this help message and exit
-  -n NAME     db filename
-  -s SCHEMA   db schema filename
+  -h, --help    show this help message and exit
+  --db DB_FILE  имя БД
+  -s            если флаг установлен, добавлять данные коммутаторов, иначе
+                добавлять DHCP записи
+
+
+$ python parse_dhcp_snooping.py get -h
+usage: parse_dhcp_snooping.py get [-h] [--db DB_FILE]
+                                  [-k {mac,ip,vlan,interface,switch}]
+                                  [-v VALUE] [-a]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --db DB_FILE          имя БД
+  -k {mac,ip,vlan,interface,switch}
+                        параметр для поиска записей
+  -v VALUE              значение параметра
+  -a                    показать все содержимое БД
 
 
 $ python parse_dhcp_snooping.py create_db
-Creating DB dhcp_snooping.db with DB schema dhcp_snooping_schema.sql
-Creating schema...
-Done
+Создаю БД dhcp_snooping.db со схемой dhcp_snooping_schema.sql
+Создаю базу данных...
 
-$ python parse_dhcp_snooping.py add sw1_dhcp_snooping.txt sw2_dhcp_snooping.txt sw3_dhcp_snooping.txt
-Reading info from file(s)
+
+$ python parse_dhcp_snooping.py add sw[1-3]_dhcp_snooping.txt
+Читаю информацию из файлов
 sw1_dhcp_snooping.txt, sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt
 
-Adding data to db dhcp_snooping.db
+Добавляю данные по DHCP записях в dhcp_snooping.db
 
 
 $ python parse_dhcp_snooping.py add -s switches.yml
-Adding switch data to database
-
+Добавляю данные о коммутаторах
 
 $ python parse_dhcp_snooping.py get
-Showing dhcp_snooping.db content...
-----------------------------------------------------------------------
-00:09:BB:3D:D6:58  10.1.10.2         10    FastEthernet0/1      sw1
-00:04:A3:3E:5B:69  10.1.5.2          5     FastEthernet0/10     sw1
-00:05:B3:7E:9B:60  10.1.5.4          5     FastEthernet0/9      sw1
-00:07:BC:3F:A6:50  10.1.10.6         10    FastEthernet0/3      sw1
-00:09:BC:3F:A6:50  192.168.1.100     100   FastEthernet0/5      sw1
-00:A9:BB:3D:D6:58  10.1.10.20        10    FastEthernet0/7      sw2
-00:B4:A3:3E:5B:69  10.1.5.20         5     FastEthernet0/5      sw2
-00:C5:B3:7E:9B:60  10.1.5.40         5     FastEthernet0/9      sw2
-00:A9:BC:3F:A6:50  100.1.1.6         3     FastEthernet0/20     sw3
+В таблице dhcp такие записи:
+
+Активные записи:
+
+-----------------  ---------------  --  ----------------  ---  -  -------------------
+00:09:BB:3D:D6:58  10.1.10.2        10  FastEthernet0/1   sw1  1  2019-03-08 16:47:52
+00:04:A3:3E:5B:69  10.1.5.2          5  FastEthernet0/10  sw1  1  2019-03-08 16:47:52
+00:05:B3:7E:9B:60  10.1.5.4          5  FastEthernet0/9   sw1  1  2019-03-08 16:47:52
+00:07:BC:3F:A6:50  10.1.10.6        10  FastEthernet0/3   sw1  1  2019-03-08 16:47:52
+00:09:BC:3F:A6:50  192.168.100.100   1  FastEthernet0/7   sw1  1  2019-03-08 16:47:52
+00:A9:BB:3D:D6:58  10.1.10.20       10  FastEthernet0/7   sw2  1  2019-03-08 16:47:52
+00:B4:A3:3E:5B:69  10.1.5.20         5  FastEthernet0/5   sw2  1  2019-03-08 16:47:52
+00:C5:B3:7E:9B:60  10.1.5.40         5  FastEthernet0/9   sw2  1  2019-03-08 16:47:52
+00:A9:BC:3F:A6:50  10.1.10.60       20  FastEthernet0/2   sw2  1  2019-03-08 16:47:52
+00:E9:BC:3F:A6:50  100.1.1.6         3  FastEthernet0/20  sw3  1  2019-03-08 16:47:52
+-----------------  ---------------  --  ----------------  ---  -  -------------------
+
 
 $ python parse_dhcp_snooping.py get -k vlan -v 10
-Geting data from DB: dhcp_snooping.db
-Request data for host(s) with vlan 10
+Данные из БД: dhcp_snooping.db
+Информация об устройствах с такими параметрами: vlan 10
 
-Detailed information for host(s) with vlan 10
-----------------------------------------
-mac         : 00:09:BB:3D:D6:58
-ip          : 10.1.10.2
-interface   : FastEthernet0/1
-switch      : sw1
-----------------------------------------
-mac         : 00:07:BC:3F:A6:50
-ip          : 10.1.10.6
-interface   : FastEthernet0/3
-switch      : sw1
-----------------------------------------
-mac         : 00:A9:BB:3D:D6:58
-ip          : 10.1.10.20
-interface   : FastEthernet0/7
-switch      : sw2
-----------------------------------------
+Активные записи:
+
+-----------------  ----------  --  ---------------  ---  -  -------------------
+00:09:BB:3D:D6:58  10.1.10.2   10  FastEthernet0/1  sw1  1  2019-03-08 16:47:52
+00:07:BC:3F:A6:50  10.1.10.6   10  FastEthernet0/3  sw1  1  2019-03-08 16:47:52
+00:A9:BB:3D:D6:58  10.1.10.20  10  FastEthernet0/7  sw2  1  2019-03-08 16:47:52
+-----------------  ----------  --  ---------------  ---  -  -------------------
 
 
 $ python parse_dhcp_snooping.py get -k vln -v 10
