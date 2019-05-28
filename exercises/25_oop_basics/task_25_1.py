21c5fde4b74cebfc8342575f978c8f340139fcae
natenka <nataliya.samoylenko@gmail.com>
2019-05-28 14:00:42 +0000

    Chapter 25 tests. Done

--- exercises/25_oop_basics/task_25_1.py
+++ exercises/25_oop_basics/task_25_1.py
@@ -1,19 +1,43 @@
+# -*- coding: utf-8 -*-
+
+'''
+Задание 25.1
+
+Создать класс Topology, который представляет топологию сети.
+
+При создании экземпляра класса, как аргумент передается словарь, который описывает топологию.
+Словарь может содержать дублирующиеся соединения.
+
+Дублем считаются такие соединения:
+('R1', 'Eth0/0'): ('SW1', 'Eth0/1') и ('SW1', 'Eth0/1'): ('R1', 'Eth0/0')
+
+В каждом экземпляре должна быть создана переменная topology, в которой содержится словарь топологии, но уже без дублей.
+
+Пример создания экземпляра класса:
+In [2]: top = Topology(topology_example)
+
+После этого, должна быть доступна переменная topology:
+
+In [3]: top.topology
+Out[3]:
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+ ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+ ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+ ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+ ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+ ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
 
 
-# Все отлично
+'''
 
-# вариант решения
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
 
-class Topology:
-    def __init__(self, topology_dict):
-        self.topo = {}
-        for local, remote in topology_dict.items():
-            if not self.topo.get(remote) == local:
-                self.topo[local] = remote
-    def topology(self):
-        pass
 
-
-if __name__ == "__main__":
-    top = Topology(topology_example)
-    print(top.topology)
