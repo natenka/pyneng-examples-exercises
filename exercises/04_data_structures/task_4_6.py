b8d1f1f954d7da0358c7d4068191a18ebd4f9628
natenka <nataliya.samoylenko@gmail.com>
2019-07-14 17:16:47 +0000

    small fixes

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -8,7 +8,7 @@ Prefix:                10.0.24.0/24
 AD/Metric:             110/41
 Next-Hop:              10.0.13.3
 Last update:           3d18h
-Outbound Interface     FastEthernet0/0
+Outbound Interface:    FastEthernet0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
