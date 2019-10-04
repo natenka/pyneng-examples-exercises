9b0bdab1f48ec5c8565921ac6cbaca0482f0fb85
yaeraly <ya.eraly@gmail.com>
2019-10-04 18:50:39 +0000

    4.8 is done

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -22,14 +22,14 @@ ip = '192.168.3.1'
 ip1, ip2, ip3, ip4 = ip.split('.')
 
 show1 = f"""
-{ip1:<10} {ip2:<10} {ip3:<10} {ip4:<10}
-{int(ip1):010b} {int(ip2):010b} {int(ip3):010b} {int(ip4):010b}
+{ip1:<8}  {ip2:<8}  {ip3:<8}  {ip4:<8}
+{int(ip1):08b}  {int(ip2):08b}  {int(ip3):08b}  {int(ip4):08b}
 """
 print(show1)
 
 template = """
-{0:<10} {1:<10} {2:<10} {3:<10}
-{0:010b} {1:010b} {2:010b} {3:010b}
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
 """
 show2 = template.format(int(ip1), int(ip2), int(ip3), int(ip4))
 print(show2)
