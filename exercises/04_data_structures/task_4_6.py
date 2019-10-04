72a63373c7ebf92aa843b3d013caa03169edd817
yaeraly <ya.eraly@gmail.com>
2019-10-04 14:51:23 +0000

    4.6 is done

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,26 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+protocol, prefix, ad_metric, via, n_hop, l_update, intf = ospf_route.split()
+
+show1 = f"""
+Protocol:              {protocol}SPF
+Prefix:                {prefix}
+AD/Metric:             {ad_metric.strip('[]')}
+Next-Hop:              {n_hop.rstrip(',')}
+Last update:           {l_update.rstrip(',')}
+Outbound Interface:    {intf}
+"""
+print(show1)
+
+template = """
+Protocol:              {}SPF
+Prefix:                {}
+AD/Metric:             {}
+Next-Hop:              {}
+Last update:           {}
+Outbound Interface:    {}
+"""
+show2 = template.format(protocol, prefix, ad_metric.strip('[]'), n_hop.rstrip(','), l_update.rstrip(','), intf)
+print(show2)
+
