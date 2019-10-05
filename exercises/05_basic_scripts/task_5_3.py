94348cf6f8c04d8330c77e49086f3d31fa6c2544
yaeraly <ya.eraly@gmail.com>
2019-10-05 15:54:19 +0000

    5.3 is done

--- exercises/05_basic_scripts/task_5_3.py
+++ exercises/05_basic_scripts/task_5_3.py
@@ -55,3 +55,12 @@ trunk_template = [
     'switchport trunk encapsulation dot1q', 'switchport mode trunk',
     'switchport trunk allowed vlan {}'
 ]
+
+intf_mode = input('Enter interface mode (access/trunk): ')
+intf = input('Enter interface type and number (Fa0/6, Gi0/1, Fa0/1): ')
+vlan = input('Enter vlan(s): ')
+switch = {'access': access_template, 'trunk': trunk_template}
+
+print(f'\ninterface {intf}')
+print('\n'.join(switch.get(intf_mode)).format(vlan))
+
