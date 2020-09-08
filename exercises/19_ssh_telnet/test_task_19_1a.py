f1538dfc01240e93e2f13143d2b013a5370e654e
natenka <nataliya.samoylenko@gmail.com>
2020-09-08 15:35:41 +0000

    Обновлен тест 19.1a: текст ошибки более общий

--- exercises/19_ssh_telnet/test_task_19_1a.py
+++ exercises/19_ssh_telnet/test_task_19_1a.py
@@ -25,7 +25,7 @@ def test_function_return_value(capsys, first_router_wrong_pass):
     Проверка работы функции
     """
     return_value = task_19_1a.send_show_command(first_router_wrong_pass, "sh ip int br")
-    correct_stdout = "Authentication failure: unable to connect"
+    correct_stdout = "Authentication fail"
     out, err = capsys.readouterr()
     assert out != "", "Сообщение об ошибке не выведено на stdout"
     assert correct_stdout in out, "Выведено неправильное сообщение об ошибке"
