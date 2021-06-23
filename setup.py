bcd6e4c84d53698b9e23bf7b58eca2d2af6241ec
Natasha Samoylenko <nataliya.samoylenko@gmail.com>
2021-06-23 15:44:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         "Click",
         "pytest",
-        "pytest-clarity",
+        "pytest-clarity==0.3.0a0",
         "pytest-json-report",
         "six",
     ],
